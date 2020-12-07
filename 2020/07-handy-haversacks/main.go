0efe0e9ddcacc8108301fe443d19d85c136da736
Jonas Stendahl <jonas.stendahl@gmail.com>
2020-12-07 20:53:47 +0100

    Optimize input parsing

--- 2020/07-handy-haversacks/main.go
+++ 2020/07-handy-haversacks/main.go
@@ -16,6 +16,13 @@ type Bag struct {
 	children []*Bag
 }
 
+type Match struct {
+	parent   *Bag
+	children [][]string
+}
+
+const ourBagColor = "shiny gold"
+
 var parentBagRegex *regexp.Regexp
 var childBagRegex *regexp.Regexp
 
@@ -36,27 +43,23 @@ func main() {
 }
 
 func SolvePart1(input string) (string, error) {
-	bags, err := parseBags(input)
+	ourBag, err := parseBags(input)
 
 	if err != nil {
 		return "", err
 	}
 
-	shinyBag := bagByColor(bags, "shiny gold")
-
-	return strconv.Itoa(parentCount(shinyBag, make(map[string]bool, 0))), nil
+	return strconv.Itoa(parentCount(ourBag, make(map[string]bool, 0))), nil
 }
 
 func SolvePart2(input string) (string, error) {
-	bags, err := parseBags(input)
+	ourBag, err := parseBags(input)
 
 	if err != nil {
 		return "", err
 	}
 
-	shinyBag := bagByColor(bags, "shiny gold")
-
-	return strconv.Itoa(childrenCount(shinyBag, make(map[string]int, 0))), nil
+	return strconv.Itoa(childrenCount(ourBag, make(map[string]int, 0))), nil
 }
 
 func bagByColor(bags map[string]*Bag, color string) *Bag {
@@ -95,30 +98,40 @@ func parentCount(bag *Bag, colors map[string]bool) (count int) {
 	return count
 }
 
-func parseBags(input string) (map[string]*Bag, error) {
+func parseBags(input string) (*Bag, error) {
 	rules := utils.ToStringSlice(input, "\n")
-	bags := make(map[string]*Bag, len(rules))
+	rulesLen := len(rules)
+	bags := make(map[string]*Bag, rulesLen)
+	matches := make([]*Match, rulesLen)
 
-	for _, r := range rules {
+	for i, r := range rules {
 		parts := strings.SplitN(r, " contain ", 2)
 		parentMatch := parentBagRegex.FindStringSubmatch(parts[0])
-		childMatches := childBagRegex.FindAllStringSubmatch(parts[1], -1)
-		parentBag := bagByColor(bags, parentMatch[1])
+		bags[parentMatch[1]] = &Bag{
+			color:    parentMatch[1],
+			children: make([]*Bag, 0),
+		}
+		matches[i] = &Match{
+			parent:   bags[parentMatch[1]],
+			children: childBagRegex.FindAllStringSubmatch(parts[1], -1),
+		}
+	}
 
-		for _, child := range childMatches {
-			childBag := bagByColor(bags, child[2])
-			childBag.parents = append(childBag.parents, parentBag)
-			childCount, err := strconv.Atoi(child[1])
+	for _, m := range matches {
+		for _, c := range m.children {
+			childBag := bags[c[2]]
+			childBag.parents = append(childBag.parents, m.parent)
+			childCount, err := strconv.Atoi(c[1])
 
 			if err != nil {
-				return bags, err
+				return nil, err
 			}
 
 			for i := 0; i < childCount; i++ {
-				parentBag.children = append(parentBag.children, childBag)
+				m.parent.children = append(m.parent.children, childBag)
 			}
 		}
 	}
 
-	return bags, nil
+	return bags[ourBagColor], nil
 }
