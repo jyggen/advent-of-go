24d47f03607c5fe1f64899acf78abd332063110b
Jonas Stendahl <jonas.stendahl@gmail.com>
2021-12-01 12:15:26 +0100

    Add solution for 2021.01

---
+++ 2021/01-sonar-sweep/main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"fmt"
+	"github.com/jyggen/advent-of-go/solver"
+	"github.com/jyggen/advent-of-go/utils"
+	"os"
+	"strconv"
+)
+
+func main() {
+	p1, p2, err := solver.SolveFromFile(os.Stdin, SolvePart1, SolvePart2)
+
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(p1)
+	fmt.Println(p2)
+}
+
+func SolvePart1(input string) (string, error) {
+	intSlice, err := utils.ToIntegerSlice(input, "\n")
+
+	if err != nil {
+		return "", err
+	}
+
+	result := 0
+
+	for i := 1; i < len(intSlice); i++ {
+		if intSlice[i] > intSlice[i-1] {
+			result++
+		}
+	}
+
+	return strconv.Itoa(result), err
+}
+
+func SolvePart2(input string) (string, error) {
+	intSlice, err := utils.ToIntegerSlice(input, "\n")
+
+	if err != nil {
+		return "", err
+	}
+
+	result := 0
+
+	for i := 0; i < len(intSlice) - 3; i++ {
+		if intSlice[i+1] + intSlice[i+2] + intSlice[i+3] > intSlice[i] + intSlice[i+1] + intSlice[i+2] {
+			result++
+		}
+	}
+
+	return strconv.Itoa(result), err
+}
---
+++ 2021/01-sonar-sweep/main_test.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"fmt"
+	"github.com/jyggen/advent-of-go/solver"
+	"testing"
+)
+
+var testCases = []*solver.TestCase{
+	{
+		Input: "199\n200\n208\n210\n200\n207\n240\n269\n260\n263",
+		Solvers: []*solver.TestCaseSolver{
+			{
+				Solver: SolvePart1,
+				Output: "7",
+			},
+			{
+				Solver: SolvePart2,
+				Output: "5",
+			},
+		},
+	},
+	{
+		Input: solver.InputFromFile("input.txt"),
+		Short: true,
+		Solvers: []*solver.TestCaseSolver{
+			{
+				Solver: SolvePart1,
+				Output: "1529",
+			},
+			{
+				Solver: SolvePart2,
+				Output: "1567",
+			},
+		},
+	},
+}
+
+func BenchmarkSolvers(b *testing.B) {
+	for i, testCase := range testCases {
+		b.Run(fmt.Sprint(i), func(subtest *testing.B) {
+			testCase.Benchmark(subtest)
+		})
+	}
+}
+
+func TestSolvers(t *testing.T) {
+	for i, testCase := range testCases {
+		t.Run(fmt.Sprint(i), func(subtest *testing.T) {
+			testCase.Test(subtest)
+		})
+	}
+}
