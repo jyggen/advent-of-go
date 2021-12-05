0d2e7998f246e89b3bfe8a0968117e6e1131a13f
Jonas Stendahl <jonas.stendahl@gmail.com>
2021-12-05 21:49:25 +0100

    Add solution for 2021.05

---
+++ 2021/05-hydrothermal-venture/main.go
@@ -0,0 +1,161 @@
+package main
+
+import (
+	"fmt"
+	"github.com/jyggen/advent-of-go/solver"
+	"github.com/jyggen/advent-of-go/utils"
+	"os"
+	"strconv"
+)
+
+func main() {
+	p1, p2, err := solver.SolveFromFile(os.Stdin, SolvePart1, SolvePart2)
+
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(p1)
+	fmt.Println(p2)
+}
+
+type line struct {
+	x *pair
+	y *pair
+}
+
+func (l *line) IsStraight() bool {
+	return l.x.IsStraight() || l.y.IsStraight()
+}
+
+type pair struct {
+	from int
+	to   int
+}
+
+func (p *pair) Check(v int) bool {
+	if p.from < p.to {
+		return v <= p.to
+	}
+
+	return v >= p.to
+}
+
+func (p *pair) Next(v int) int {
+	if p.from < p.to {
+		return v + 1
+	}
+
+	return v - 1
+}
+
+func (p *pair) IsStraight() bool {
+	return p.to == p.from
+}
+
+func SolvePart1(input string) (string, error) {
+	grid, lines, err := makeGrid(input)
+
+	if err != nil {
+		return "", err
+	}
+
+	for _, l := range lines {
+		if !l.IsStraight() {
+			continue
+		}
+
+		for x := l.x.from; l.x.Check(x); x = l.x.Next(x) {
+			for y := l.y.from; l.y.Check(y); y = l.y.Next(y) {
+				grid[y][x]++
+			}
+		}
+	}
+
+	sum := 0
+
+	for _, row := range grid {
+		for _, col := range row {
+			if col >= 2 {
+				sum++
+			}
+		}
+	}
+
+	return strconv.Itoa(sum), nil
+}
+
+func SolvePart2(input string) (string, error) {
+	grid, lines, err := makeGrid(input)
+
+	if err != nil {
+		return "", err
+	}
+
+	for _, l := range lines {
+		if !l.IsStraight() {
+			for x, y := l.x.from, l.y.from; l.x.Check(x) && l.y.Check(y); x, y = l.x.Next(x), l.y.Next(y) {
+				grid[y][x]++
+			}
+		} else {
+			for x := l.x.from; l.x.Check(x); x = l.x.Next(x) {
+				for y := l.y.from; l.y.Check(y); y = l.y.Next(y) {
+					grid[y][x]++
+				}
+			}
+		}
+	}
+
+	sum := 0
+
+	for _, row := range grid {
+		for _, col := range row {
+			if col >= 2 {
+				sum++
+			}
+		}
+	}
+
+	return strconv.Itoa(sum), nil
+}
+
+func makeGrid(input string) ([][]int, []*line, error) {
+	stringSlice := utils.ToStringSlice(input, "\n")
+	lines := make([]*line, len(stringSlice))
+	maxX := 0
+	maxY := 0
+
+	for i, row := range stringSlice {
+		var x1, y1, x2, y2 int
+
+		if _, err := fmt.Sscanf(row, "%d,%d -> %d,%d", &x1, &y1, &x2, &y2); err != nil {
+			return make([][]int, 0), lines, err
+		}
+
+		if x1 > maxX {
+			maxX = x1
+		}
+
+		if x2 > maxX {
+			maxX = x2
+		}
+
+		if y1 > maxY {
+			maxY = y1
+		}
+
+		if y2 > maxY {
+			maxY = y2
+		}
+
+		lines[i] = &line{&pair{from: x1, to: x2}, &pair{from: y1, to: y2}}
+	}
+
+	grid := make([][]int, maxY+1)
+
+	for y := range grid {
+		grid[y] = make([]int, maxX+1)
+	}
+
+	return grid, lines, nil
+}
---
+++ 2021/05-hydrothermal-venture/main_test.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"fmt"
+	"github.com/jyggen/advent-of-go/solver"
+	"testing"
+)
+
+var testCases = []*solver.TestCase{
+	{
+		Input: "0,9 -> 5,9\n8,0 -> 0,8\n9,4 -> 3,4\n2,2 -> 2,1\n7,0 -> 7,4\n6,4 -> 2,0\n0,9 -> 2,9\n3,4 -> 1,4\n0,0 -> 8,8\n5,5 -> 8,2",
+		Solvers: []*solver.TestCaseSolver{
+			{
+				Solver: SolvePart1,
+				Output: "5",
+			},
+			{
+				Solver: SolvePart2,
+				Output: "12",
+			},
+		},
+	},
+	/*{
+		Input: solver.InputFromFile("input.txt"),
+		Short: true,
+		Solvers: []*solver.TestCaseSolver{
+			{
+				Solver: SolvePart1,
+				Output: "63552",
+			},
+			{
+				Solver: SolvePart2,
+				Output: "9020",
+			},
+		},
+	},*/
+}
+
+func BenchmarkSolvers(b *testing.B) {
+	for i, testCase := range testCases {
+		b.Run(fmt.Sprint(i), func(subtest *testing.B) {
+			testCase.Benchmark(subtest)
+		})
+	}
+}
+
+func TestSolvers(t *testing.T) {
+	for i, testCase := range testCases {
+		t.Run(fmt.Sprint(i), func(subtest *testing.T) {
+			testCase.Test(subtest)
+		})
+	}
+}
