50629440885439c7cb7a0775ec4c03d49c617c78
Jonas Stendahl <jonas.stendahl@gmail.com>
2021-12-11 15:49:49 +0100

    Add solution 2021.11

---
+++ 2021/11-dumbo-octopus/main.go
@@ -0,0 +1,143 @@
+package main
+
+import (
+	"container/list"
+	"fmt"
+	"os"
+	"strconv"
+
+	"github.com/jyggen/advent-of-go/internal/solver"
+	"github.com/jyggen/advent-of-go/internal/utils"
+)
+
+func main() {
+	p1, p2, err := solver.SolveFromFile(os.Stdin, SolvePart1, SolvePart2)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(p1)
+	fmt.Println(p2)
+}
+
+type grid struct {
+	colLength int
+	grid      []int
+	rowLength int
+}
+
+func (g *grid) simulate() int {
+	queue := list.New()
+	flashes := 0
+
+	for j := 0; j < len(g.grid); j++ {
+		queue.PushBack(j)
+	}
+
+	for queue.Len() > 0 {
+		e := queue.Front()
+		j := e.Value.(int)
+
+		g.grid[j]++
+
+		if g.grid[j] == 10 {
+			flashes++
+
+			x, y := utils.ToCoordinates(j, g.colLength)
+
+			for _, k := range []int{
+				utils.Neighbour(x, y, utils.North, g.rowLength, g.colLength),
+				utils.Neighbour(x, y, utils.NorthEast, g.rowLength, g.colLength),
+				utils.Neighbour(x, y, utils.East, g.rowLength, g.colLength),
+				utils.Neighbour(x, y, utils.SouthEast, g.rowLength, g.colLength),
+				utils.Neighbour(x, y, utils.South, g.rowLength, g.colLength),
+				utils.Neighbour(x, y, utils.SouthWest, g.rowLength, g.colLength),
+				utils.Neighbour(x, y, utils.West, g.rowLength, g.colLength),
+				utils.Neighbour(x, y, utils.NorthWest, g.rowLength, g.colLength),
+			} {
+				if k != -1 {
+					queue.PushBack(k)
+				}
+			}
+		}
+
+		queue.Remove(e)
+	}
+
+	return flashes
+}
+
+func makeGrid(input string) (*grid, error) {
+	stringSlice := utils.ToStringSlice(input, "\n")
+	colLength := len(stringSlice[0])
+	rowLength := len(stringSlice)
+	g := &grid{
+		colLength: colLength,
+		grid:      make([]int, len(stringSlice)*colLength),
+		rowLength: rowLength,
+	}
+
+	for i, s := range stringSlice {
+		ints, err := utils.ToIntegerSlice(s, "")
+
+		if err != nil {
+			return g, err
+		}
+
+		for j, digit := range ints {
+			g.grid[(i*colLength)+j] = digit
+		}
+	}
+
+	return g, nil
+}
+
+func SolvePart1(input string) (string, error) {
+	g, err := makeGrid(input)
+
+	if err != nil {
+		return "", err
+	}
+
+	flashes := 0
+
+	for i := 0; i < 100; i++ {
+		flashes += g.simulate()
+
+		for k, v := range g.grid {
+			if v > 9 {
+				g.grid[k] = 0
+			}
+		}
+	}
+
+	return strconv.Itoa(flashes), nil
+}
+
+func SolvePart2(input string) (string, error) {
+	g, err := makeGrid(input)
+
+	if err != nil {
+		return "", err
+	}
+
+	for i := 0; ; i++ {
+		g.simulate()
+
+		flashed := true
+
+		for k, v := range g.grid {
+			if v > 9 {
+				g.grid[k] = 0
+			} else {
+				flashed = false
+			}
+		}
+
+		if flashed {
+			return strconv.Itoa(i + 1), nil
+		}
+	}
+
+	return strconv.Itoa(0), nil
+}
---
+++ 2021/11-dumbo-octopus/main_test.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/jyggen/advent-of-go/internal/solver"
+)
+
+var testCases = []*solver.TestCase{
+	{
+		Input: "5483143223\n2745854711\n5264556173\n6141336146\n6357385478\n4167524645\n2176841721\n6882881134\n4846848554\n5283751526",
+		Solvers: []*solver.TestCaseSolver{
+			{
+				Solver: SolvePart1,
+				Output: "1656",
+			},
+			{
+				Solver: SolvePart2,
+				Output: "195",
+			},
+		},
+	},
+	{
+		Input: solver.InputFromFile("input.txt"),
+		Short: true,
+		Solvers: []*solver.TestCaseSolver{
+			{
+				Solver: SolvePart1,
+				Output: "1655",
+			},
+			{
+				Solver: SolvePart2,
+				Output: "337",
+			},
+		},
+	},
+}
+
+func BenchmarkSolvers(b *testing.B) {
+	for i, testCase := range testCases {
+		b.Run(fmt.Sprint(i), func(subtest *testing.B) {
+			testCase.Benchmark(subtest)
+		})
+	}
+}
+
+func TestSolvers(t *testing.T) {
+	for i, testCase := range testCases {
+		t.Run(fmt.Sprint(i), func(subtest *testing.T) {
+			testCase.Test(subtest)
+		})
+	}
+}
