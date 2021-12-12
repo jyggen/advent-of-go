2b05cfca92313eac1d82ab2e314501d408956c47
Jonas Stendahl <jonas.stendahl@gmail.com>
2021-12-12 22:13:54 +0100

    test: add tests for utils

--- internal/utils/main_test.go
+++ internal/utils/main_test.go
@@ -18,3 +18,42 @@ func TestCombinations(t *testing.T) {
 		{'b', 'b', 'b'},
 	}, Combinations([]rune{'a', 'b'}, 3))
 }
+
+func BenchmarkToIntegerSlice(b *testing.B) {
+	input := "1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20"
+	for i := 0; i < b.N; i++ {
+		ToIntegerSlice(input, ",")
+	}
+}
+
+func BenchmarkToRuneSlice(b *testing.B) {
+	input := "abc,def,ghi,jkl,mno,pqr,stu,vwx,yz"
+	for i := 0; i < b.N; i++ {
+		ToRuneSlice(input, ",")
+	}
+}
+
+func BenchmarkToStringSlice(b *testing.B) {
+	input := "a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z"
+	for i := 0; i < b.N; i++ {
+		ToStringSlice(input, ",")
+	}
+}
+
+func TestToIntegerSlice(t *testing.T) {
+	result, _ := ToIntegerSlice("1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20", ",")
+
+	assert.Equal(t, []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20}, result)
+}
+
+func TestToRuneSlice(t *testing.T) {
+	result := ToRuneSlice("abc,def,ghi,jkl,mno,pqr,stu,vwx,yz", ",")
+
+	assert.Equal(t, [][]rune{{'a', 'b', 'c'}, {'d', 'e', 'f'}, {'g', 'h', 'i'}, {'j', 'k', 'l'}, {'m', 'n', 'o'}, {'p', 'q', 'r'}, {'s', 't', 'u'}, {'v', 'w', 'x'}, {'y', 'z'}}, result)
+}
+
+func TestToStringSlice(t *testing.T) {
+	result := ToStringSlice("a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z", ",")
+
+	assert.Equal(t, []string{"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"}, result)
+}
