bfe1e22a87f1a081aa4b46feca84bf55a577defc
Jonas Stendahl <jonas@stendahl.me>
2022-12-05 15:11:14 +0100

    perf(2022.05): reuse solution between p1 and p2

--- 2022/05-supply-stacks/main.go
+++ 2022/05-supply-stacks/main.go
@@ -39,41 +39,20 @@ func parseInput(input string) ([][]uint8, []int) {
 	return columns, utils.ToOptimisticIntSlice(sections[1])
 }
 
-func SolvePart1(input string) (string, error) {
-	columns, integers := parseInput(input)
+func arrangeCrates(columns [][]uint8, commands []int, stacked bool) string {
+	for i := 0; i < len(commands); i += 3 {
+		count, from, to := commands[i], commands[i+1]-1, commands[i+2]-1
+		movedCrates := make([]uint8, len(columns[from][:count]))
 
-	var crate uint8
+		copy(movedCrates, columns[from][:count])
 
-	for i := 0; i < len(integers); i += 3 {
-		from, to := integers[i+1]-1, integers[i+2]-1
-
-		for j := 0; j < integers[i]; j++ {
-			crate, columns[from] = columns[from][0], columns[from][1:]
-			columns[to] = append([]uint8{crate}, columns[to]...)
+		if !stacked {
+			for j, k := 0, len(movedCrates)-1; j < k; j, k = j+1, k-1 {
+				movedCrates[j], movedCrates[k] = movedCrates[k], movedCrates[j]
+			}
 		}
-	}
 
-	result := make([]uint8, 0, len(columns))
-
-	for _, column := range columns {
-		if len(column) != 0 {
-			result = append(result, column[0])
-		}
-	}
-
-	return string(result), nil
-}
-
-func SolvePart2(input string) (string, error) {
-	columns, integers := parseInput(input)
-
-	for i := 0; i < len(integers); i += 3 {
-		from, to := integers[i+1]-1, integers[i+2]-1
-		movedCrates := make([]uint8, len(columns[from][:integers[i]]))
-
-		copy(movedCrates, columns[from][:integers[i]])
-
-		columns[from] = columns[from][integers[i]:]
+		columns[from] = columns[from][count:]
 		columns[to] = append(movedCrates, columns[to]...)
 	}
 
@@ -85,5 +64,17 @@ func SolvePart2(input string) (string, error) {
 		}
 	}
 
-	return string(result), nil
+	return string(result)
+}
+
+func SolvePart1(input string) (string, error) {
+	columns, commands := parseInput(input)
+
+	return arrangeCrates(columns, commands, false), nil
+}
+
+func SolvePart2(input string) (string, error) {
+	columns, commands := parseInput(input)
+
+	return arrangeCrates(columns, commands, true), nil
 }
