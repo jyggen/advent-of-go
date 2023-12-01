e36dfe883627ede741197d08211a1dcc9e05b3dc
Jonas Stendahl <jonas@stendahl.me>
2023-12-01 14:11:56 +0100

    fix: remove unused code

--- 2023/01-trebuchet/main.go
+++ 2023/01-trebuchet/main.go
@@ -10,36 +10,10 @@ import (
 	"github.com/jyggen/advent-of-go/internal/utils"
 )
 
-var digitStrings = map[string]rune{
-	"one":   '1',
-	"two":   '2',
-	"three": '3',
-	"four":  '4',
-	"five":  '5',
-	"six":   '6',
-	"seven": '7',
-	"eight": '8',
-	"nine":  '9',
-}
-
 var firstReplacer *strings.Replacer
 var lastReplacer *strings.Replacer
 
-var digitStringsReverse map[string]rune
-
 func init() {
-	digitStringsReverse = make(map[string]rune, len(digitStrings))
-
-	for k, v := range digitStrings {
-		reversed := []rune(k)
-
-		for i, j := 0, len(reversed)-1; i < j; i, j = i+1, j-1 {
-			reversed[i], reversed[j] = reversed[j], reversed[i]
-		}
-
-		digitStringsReverse[string(reversed)] = v
-	}
-
 	firstReplacer = strings.NewReplacer("one", "1", "two", "2", "three", "3", "four", "4", "five", "5", "six", "6", "seven", "7", "eight", "8", "nine", "9")
 	lastReplacer = strings.NewReplacer("eno", "1", "owt", "2", "eerht", "3", "ruof", "4", "evif", "5", "xis", "6", "neves", "7", "thgie", "8", "enin", "9")
 }
@@ -52,18 +26,6 @@ func main() {
 
 	fmt.Println(p1)
 	fmt.Println(p2)
-}
-
-func containsDigitString(builder strings.Builder, digitStringsMap map[string]rune) (bool, rune) {
-	maybeDigitString := builder.String()
-
-	for letter, r := range digitStringsMap {
-		if strings.HasSuffix(maybeDigitString, letter) {
-			return true, r
-		}
-	}
-
-	return false, 0
 }
 
 func SolvePart1(input string) (string, error) {
