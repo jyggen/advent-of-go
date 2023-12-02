1a7221e2a22376be0f7b209fae6cc1ee42bcc0a5
Jonas Stendahl <jonas@stendahl.me>
2023-12-02 13:07:24 +0100

    perf(2023.02): parse using less iterations

--- 2023/02-cube-conundrum/main.go
+++ 2023/02-cube-conundrum/main.go
@@ -9,15 +9,11 @@ import (
 	"strings"
 )
 
-type round struct {
-	blue  int
-	green int
-	red   int
-}
-
 type game struct {
-	number int
-	rounds []round
+	highBlue  int
+	highGreen int
+	highRed   int
+	number    int
 }
 
 func main() {
@@ -34,30 +30,28 @@ func parse(rows []string) []game {
 	games := make([]game, 0, len(rows))
 
 	for _, row := range rows {
-		parts := strings.SplitN(row, ": ", 2)
-		sets := strings.Split(parts[1], "; ")
-		gameNo, _ := strconv.Atoi(parts[0][5:])
-		g := game{number: gameNo, rounds: make([]round, 0, len(sets))}
+		colonIndex := strings.Index(row, ": ")
+		gameNo, _ := strconv.Atoi(row[5:colonIndex])
+		g := game{number: gameNo}
+		cubes := strings.Split(row[colonIndex+2:], " ")
 
-		for _, s := range sets {
-			cubes := strings.Split(s, ", ")
-			r := round{}
+		for i := 0; i < len(cubes); i += 2 {
+			count, _ := strconv.Atoi(cubes[i])
 
-			for _, c := range cubes {
-				details := strings.SplitN(c, " ", 2)
-				count, _ := strconv.Atoi(details[0])
-
-				switch details[1] {
-				case "blue":
-					r.blue = count
-				case "green":
-					r.green = count
-				case "red":
-					r.red = count
+			switch cubes[i+1] {
+			case "blue", "blue,", "blue;":
+				if count > g.highBlue {
+					g.highBlue = count
+				}
+			case "green", "green,", "green;":
+				if count > g.highGreen {
+					g.highGreen = count
+				}
+			case "red", "red,", "red;":
+				if count > g.highRed {
+					g.highRed = count
 				}
 			}
-
-			g.rounds = append(g.rounds, r)
 		}
 
 		games = append(games, g)
@@ -72,15 +66,7 @@ func SolvePart1(input string) (string, error) {
 	valid := 0
 
 	for _, g := range games {
-		ok := true
-		for _, r := range g.rounds {
-			if r.blue > 14 || r.green > 13 || r.red > 12 {
-				ok = false
-				break
-			}
-		}
-
-		if ok {
+		if g.highBlue <= 14 && g.highGreen <= 13 && g.highRed <= 12 {
 			valid += g.number
 		}
 	}
@@ -94,23 +80,7 @@ func SolvePart2(input string) (string, error) {
 	sum := 0
 
 	for _, g := range games {
-		var highBlue, highGreen, highRed int
-
-		for _, r := range g.rounds {
-			if highBlue < r.blue {
-				highBlue = r.blue
-			}
-
-			if highGreen < r.green {
-				highGreen = r.green
-			}
-
-			if highRed < r.red {
-				highRed = r.red
-			}
-		}
-
-		sum += highBlue * highGreen * highRed
+		sum += g.highBlue * g.highGreen * g.highRed
 	}
 
 	return strconv.Itoa(sum), nil
