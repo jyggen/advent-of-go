6bfc0f715bdc633926d708ede1995c8511edc1f7
Jonas Stendahl <jonas@stendahl.me>
2023-12-07 11:56:29 +0100

    refactor(2023.07): dedupe code

--- 2023/07-camel-cards/main.go
+++ 2023/07-camel-cards/main.go
@@ -54,74 +54,7 @@ type hand struct {
 	bid      int
 }
 
-func parseHandWithJack(input string) hand {
-	delimiterAt := strings.IndexRune(input, ' ')
-	cardIdentifiers := []rune(input[:delimiterAt])
-	bid, _ := strconv.Atoi(input[delimiterAt+1:])
-	h := hand{
-		handType: highCard,
-		cards:    make([]int, 0, len(cardIdentifiers)),
-		bid:      bid,
-	}
-
-	counts := make([]int, 13)
-
-	for i, c := range cardIdentifiers {
-		switch c {
-		case 'A':
-			h.cards = append(h.cards, ace)
-		case 'K':
-			h.cards = append(h.cards, king)
-		case 'Q':
-			h.cards = append(h.cards, queen)
-		case 'J':
-			h.cards = append(h.cards, jack)
-		case 'T':
-			h.cards = append(h.cards, ten)
-		case '9':
-			h.cards = append(h.cards, nine)
-		case '8':
-			h.cards = append(h.cards, eight)
-		case '7':
-			h.cards = append(h.cards, seven)
-		case '6':
-			h.cards = append(h.cards, six)
-		case '5':
-			h.cards = append(h.cards, five)
-		case '4':
-			h.cards = append(h.cards, four)
-		case '3':
-			h.cards = append(h.cards, three)
-		case '2':
-			h.cards = append(h.cards, two)
-		}
-
-		counts[h.cards[i]]++
-	}
-
-	sort.Sort(sort.Reverse(sort.IntSlice(counts)))
-
-	switch {
-	case counts[0] == 5:
-		h.handType = fiveOfAKind
-	case counts[0] == 4:
-		h.handType = fourOfAKind
-	case counts[0] == 3 && counts[1] == 2:
-		h.handType = fullHouse
-	case counts[0] == 3:
-		h.handType = threeOfAKind
-	case counts[0] == 2 && counts[1] == 2:
-		h.handType = twoPair
-	case counts[0] == 2:
-		h.handType = onePair
-	default:
-		h.handType = highCard
-	}
-
-	return h
-}
-
-func parseHandWithJoker(input string) hand {
+func parseHand(input string, useJokers bool) hand {
 	delimiterAt := strings.IndexRune(input, ' ')
 	cardIdentifiers := []rune(input[:delimiterAt])
 	bid, _ := strconv.Atoi(input[delimiterAt+1:])
@@ -143,8 +76,12 @@ func parseHandWithJoker(input string) hand {
 		case 'Q':
 			h.cards = append(h.cards, queen)
 		case 'J':
-			h.cards = append(h.cards, joker)
-			numJokers++
+			if useJokers {
+				h.cards = append(h.cards, joker)
+				numJokers++
+			} else {
+				h.cards = append(h.cards, jack)
+			}
 		case 'T':
 			h.cards = append(h.cards, ten)
 		case '9':
@@ -165,7 +102,7 @@ func parseHandWithJoker(input string) hand {
 			h.cards = append(h.cards, two)
 		}
 
-		if c != 'J' {
+		if !useJokers || c != 'J' {
 			counts[h.cards[i]]++
 		}
 	}
@@ -192,12 +129,12 @@ func parseHandWithJoker(input string) hand {
 	return h
 }
 
-func parseHands(input string, parseHandFunc func(string) hand) []hand {
+func parseHands(input string, useJokers bool) []hand {
 	rows := utils.ToStringSlice(input, "\n")
 	hands := make([]hand, 0, len(rows))
 
 	for _, r := range rows {
-		hands = append(hands, parseHandFunc(r))
+		hands = append(hands, parseHand(r, useJokers))
 	}
 
 	sort.SliceStable(hands, func(i, j int) bool {
@@ -218,7 +155,7 @@ func parseHands(input string, parseHandFunc func(string) hand) []hand {
 }
 
 func SolvePart1(input string) (string, error) {
-	hands := parseHands(input, parseHandWithJack)
+	hands := parseHands(input, false)
 	total := 0
 
 	for i, h := range hands {
@@ -229,7 +166,7 @@ func SolvePart1(input string) (string, error) {
 }
 
 func SolvePart2(input string) (string, error) {
-	hands := parseHands(input, parseHandWithJoker)
+	hands := parseHands(input, true)
 	total := 0
 
 	for i, h := range hands {
