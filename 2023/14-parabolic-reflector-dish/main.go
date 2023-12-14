8bf0338f127babadb6e124e9591481a52e670050
Jonas Stendahl <jonas@stendahl.me>
2023-12-15 00:31:34 +0100

    feat(2023.14): add solution

---
+++ 2023/14-parabolic-reflector-dish/main.go
@@ -0,0 +1,178 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"github.com/jyggen/advent-of-go/internal/solver"
+	"github.com/jyggen/advent-of-go/internal/utils"
+	"os"
+	"strconv"
+)
+
+const (
+	empty = '.'
+	round = 'O'
+)
+
+func main() {
+	p1, p2, err := solver.SolveFromFile(os.Stdin, SolvePart1, SolvePart2)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(p1)
+	fmt.Println(p2)
+}
+
+func north(platform [][]byte) int {
+	sum := 0
+	height := len(platform)
+
+	for y, row := range platform {
+		for x, column := range row {
+			if column != round {
+				continue
+			}
+
+			y2 := y - 1
+
+			for ; y2 >= 0; y2-- {
+				if platform[y2][x] != empty {
+					break
+				}
+
+				platform[y2][x] = round
+				platform[y2+1][x] = empty
+			}
+
+			y2++
+			sum += height - y2
+		}
+	}
+
+	return sum
+}
+
+func west(platform [][]byte) int {
+	sum := 0
+	height := len(platform)
+
+	for x := 0; x < len(platform[0]); x++ {
+		for y := 0; y < height; y++ {
+			if platform[y][x] != round {
+				continue
+			}
+
+			x2 := x - 1
+
+			for ; x2 >= 0; x2-- {
+				if platform[y][x2] != empty {
+					break
+				}
+
+				platform[y][x2] = round
+				platform[y][x2+1] = empty
+			}
+
+			x2++
+			sum += height - y
+		}
+	}
+
+	return sum
+}
+
+func south(platform [][]byte) int {
+	sum := 0
+	height := len(platform)
+
+	for y := height - 1; y >= 0; y-- {
+		for x, column := range platform[y] {
+			if column != round {
+				continue
+			}
+
+			y2 := y + 1
+
+			for ; y2 < height; y2++ {
+				if platform[y2][x] != empty {
+					break
+				}
+
+				platform[y2][x] = round
+				platform[y2-1][x] = empty
+			}
+
+			y2--
+			sum += height - y2
+		}
+	}
+
+	return sum
+}
+
+func east(platform [][]byte) int {
+	sum := 0
+	height := len(platform)
+
+	for x := len(platform[0]) - 1; x >= 0; x-- {
+		for y := height - 1; y >= 0; y-- {
+			if platform[y][x] != round {
+				continue
+			}
+
+			x2 := x + 1
+
+			for ; x2 < len(platform[0]); x2++ {
+				if platform[y][x2] != empty {
+					break
+				}
+
+				platform[y][x2] = round
+				platform[y][x2-1] = empty
+			}
+
+			x2--
+			sum += height - y
+		}
+	}
+
+	return sum
+}
+
+func SolvePart1(input string) (string, error) {
+	platform := utils.ToByteSlice(input, '\n')
+
+	return strconv.Itoa(north(platform)), nil
+}
+
+func SolvePart2(input string) (string, error) {
+	platform := utils.ToByteSlice(input, '\n')
+	cycles := 1000000000
+	sum := 0
+	looped := false
+	cache := map[string]int{
+		string(bytes.Join(platform, nil)): 0,
+	}
+
+	for i := 1; i <= cycles; i++ {
+		north(platform)
+		west(platform)
+		south(platform)
+
+		sum = east(platform)
+
+		if !looped {
+			key := string(bytes.Join(platform, nil))
+
+			if v, ok := cache[key]; ok {
+				i = cycles - (cycles-v)%(i-v)
+				looped = true
+			} else {
+				cache[key] = i
+			}
+		}
+	}
+
+	return strconv.Itoa(sum), nil
+}
---
+++ 2023/14-parabolic-reflector-dish/main_test.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"strconv"
+	"testing"
+
+	"github.com/jyggen/advent-of-go/internal/solver"
+)
+
+var testCases = []*solver.TestCase{
+	{
+		Input: "O....#....\nO.OO#....#\n.....##...\nOO.#O....O\n.O.....O#.\nO.#..O.#.#\n..O..#O..O\n.......O..\n#....###..\n#OO..#....",
+		Solvers: []*solver.TestCaseSolver{
+			{
+				Solver: SolvePart1,
+				Output: "136",
+			},
+			{
+				Solver: SolvePart2,
+				Output: "64",
+			},
+		},
+	},
+	{
+		Short: true,
+		Solvers: []*solver.TestCaseSolver{
+			{
+				Solver: SolvePart1,
+				Output: "109098",
+			},
+			{
+				Solver: SolvePart2,
+				Output: "100064",
+			},
+		},
+	},
+}
+
+func BenchmarkSolvers(b *testing.B) {
+	for i, testCase := range testCases {
+		b.Run(strconv.Itoa(i), func(subtest *testing.B) {
+			testCase.Benchmark(subtest)
+		})
+	}
+}
+
+func TestSolvers(t *testing.T) {
+	t.Parallel()
+
+	for i, testCase := range testCases {
+		testCase := testCase
+
+		t.Run(strconv.Itoa(i), func(subtest *testing.T) {
+			subtest.Parallel()
+			testCase.Test(subtest)
+		})
+	}
+}
